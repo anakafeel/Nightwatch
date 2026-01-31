c3fa99c54c8c6a5270ef717f3e0a492158fc02bf
senuradissa <senuradissa@hotmail.com>
2026-01-31 17:44:12 -0500

    feat-add OSM query files
    Added files to query OSM and return csv file of all nodes along with
    id's

---
+++ backend/scripts/ottawa_lights_script.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+
+OVERPASS_URL = "https://overpass-api.de/api/interpreter"
+
+#OSM query
+query = """
+[out:json][timeout:120];
+node["highway"="street_lamp"](45.2,-76.1,45.6,-75.2);
+out body;
+"""
+#"""
+#[out:json] [timeout:60];
+#area["name" = "Ottawa, Ontario, Canada"]["boundary" = "administrative"] -> .searchArea;
+#(
+#    node["highway" = "street_lamp"] (area.searchArea);
+#);
+#out body;
+#"""
+
+response = requests.post(OVERPASS_URL, data=query)
+#raise error 
+response.raise_for_status()
+
+data = response.json() #return json
+print(len(data["elements"]))
+print(data["elements"][:3])
+
+streetlights = []
+
+for x in data["elements"]:
+    streetlights.append({
+        "id": x["id"],
+        "lat": x["lat"],
+        "lon": x["lon"],
+        "tags": x.get("tags", {})
+        })
+    
+#data frame
+df = pd.DataFrame(streetlights)
+tags_df = pd.json_normalize(df["tags"])
+df = df.drop(columns=["tags"]).join(tags_df)
+
+df.to_csv("ottawa_steet_lights.csv", index = False)
+
+print(f"Saved {len(df)} streetlights to ottawa_street_lights.csv")
+
+
